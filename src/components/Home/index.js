dfdc0a724397b2d89cba79e25e557b6d4d8b8e4e
Tomislav Bermanec <tbermanec@gmail.com>
2020-03-26 21:42:45 +0100

    Add Firebase Structure

---
+++ src/components/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const HOme = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
+export default Home;
---
+++ src/components/PasswordExport/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const PasswordExport = () => (
+  <div>
+    <h1>PasswordExport</h1>
+  </div>
+);
+export default PasswordExport;
