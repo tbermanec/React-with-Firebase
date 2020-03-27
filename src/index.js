5fdbae981ca801680efc29971e40457c008d3bac
Tomislav Bermanec <tbermanec@gmail.com>
2020-03-27 23:44:12 +0100

    Add Firebase Context

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './components/App/app';
 import * as serviceWorker from './serviceWorker';
 
+import App from './components/App/app';
+import Firebase, { FirebaseContext } from './components/Firebase/firebase';
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
